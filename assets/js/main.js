37605ca65c36407dcf618bbc671c2bcee4781c1a
Atikah A. Siregar <89565846+szatk@users.noreply.github.com>
2023-03-20 10:28:16 +0700

    new update

--- assets/js/main.js
+++ assets/js/main.js
@@ -172,3 +172,31 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+// JAM
+	window.setTimeout("waktu()", 1000);
+ 
+	function waktu() {
+		var dt = new Date();
+		setTimeout("waktu()", 1000);
+        document.getElementById("waktu").innerHTML = (("0"+dt.getHours()).slice(-2)) +":"+ (("0"+dt.getMinutes()).slice(-2)) +":"+ (("0"+dt.getSeconds()).slice(-2));
+        // document.getElementById("tanggal").innerHTML = (("0"+dt.getDate()).slice(-2)) +"."+ (("0"+(dt.getMonth()+1)).slice(-2)) +"."+ (dt.getFullYear()) 
+	
+        var tw = new Date();
+        if (tw.getTimezoneOffset() == 0) (a=tw.getTime() + ( 7 *60*60*1000))
+        else (a=tw.getTime());
+        tw.setTime(a);
+        var tahun= tw.getFullYear ();
+        var hari= tw.getDay ();
+        var bulan= tw.getMonth ();
+        var tanggal= (("0"+tw.getDate()).slice(-2))
+        var hariarray=new Array("Minggu,","Senin,","Selasa,","Rabu,","Kamis,","Jum'at,","Sabtu,");
+        var bulanarray=new Array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember");
+        document.getElementById("tanggal").innerHTML = hariarray[hari]+" "+tanggal+" "+bulanarray[bulan]+" "+tahun;
+    }
+
+    
+    // document.getElementById("tanggalwaktu").innerHTML = waktu.toLocaleTimeString();
+    // document.getElementById("jam").innerHTML = waktu.getHours();
+	// 	document.getElementById("menit").innerHTML = waktu.getMinutes();
+	// 	document.getElementById("detik").innerHTML = waktu.getSeconds();
